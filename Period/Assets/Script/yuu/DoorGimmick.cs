2aacc9cf2a7cdc4db00dfc4e6725908ce78279bf
増山 <analogwatch24@gmail.com>
2016-10-19 23:18:43 +0900

    [増山][追加]ドアを開けるっぽいギミック（解くヒントとなるUI表示はまだ）と回転で橋を架けるっぽいギミックの追加。

---
+++ Period/Assets/Script/yuu/DoorGimmick.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorGimmick : MonoBehaviour {
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
+    
+    public double minPassNum = 0;
+    public double maxPassNum = 10;
+    public decimal password;
+    public decimal minPass;
+    public decimal maxPass;
+    private float leftDoorMove;
+    private float rightDoorMove;
+
+
+    public bool Complete;
+
+    GimmickStateTypeD GimStateTypeD;
+	// Use this for initialization
+	void Start () {
+        GimStateTypeD = gameObject.GetComponent<GimmickStateTypeD>();
+        minPass = (decimal)minPassNum;
+        maxPass = (decimal)maxPassNum;
+        Complete = false;
+        leftDoorMove = LeftDoor.transform.position.x;
+        rightDoorMove = RightDoor.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NumGimmick();
+        OpenDoor();
+	}
+
+    void NumGimmick()
+    {
+        if (gameObject.tag == "Gimmick")
+        {
+            password = GimStateTypeD.gimmickNum;
+
+            if (minPass < password && password < maxPass)
+            {
+                Complete = true;
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (Complete == true)
+        {
+            LeftDoor.transform.position += new Vector3(leftDoorMove * Time.deltaTime, 0, 0);
+            RightDoor.transform.position += new Vector3(rightDoorMove * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Period/Assets/Script/yuu/RotationGimmick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationGimmick : MonoBehaviour {
+    public GameObject Bridge;
+    public float rotation;
+
+    GimmickStateTypeD GimStateTypeD;
+	// Use this for initialization
+	void Start () {
+	    GimStateTypeD = gameObject.GetComponent<GimmickStateTypeD>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotation = (float)GimStateTypeD.gimmickNum;
+        Bridge.transform.rotation = Quaternion.Euler(0, rotation, 0);
+	}
+}
