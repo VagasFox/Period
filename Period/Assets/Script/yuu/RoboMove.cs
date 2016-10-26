803cb96a3597985f678ed3afce03a6283870873c
増山 <analogwatch24@gmail.com>
2016-10-26 19:52:59 +0900

    [増山][追加]Roboを動かすスクリプトRoboMoveを作成。それに合わせてロボットにidle状態と移動時のrun状態のアニメーションも入れてみました。

---
+++ Period/Assets/Script/yuu/RoboMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoboMove : MonoBehaviour {
+    public float speed = 4.0f;
+    public float rotateSpeed = 5;
+    private float rotate;
+    public float gravity = 0.3f;
+    private CharacterController controller;
+    private Vector3 DirectMove = Vector3.zero;
+
+    public Animator charaAnim;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ActMove();
+        DirectMove.y -= gravity * 60 * Time.deltaTime;
+        controller.Move(DirectMove * Time.deltaTime);
+        CharaRotation();
+	}
+
+    void ActMove()
+    {
+        DirectMove = transform.forward * Input.GetAxisRaw("Vertical") * speed;
+        
+        //DirectMove.x = Input.GetAxisRaw("Horizontal") * speed;
+        charaAnim.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")) + Mathf.Abs(Input.GetAxisRaw("Vertical")));
+    }
+
+    void CharaRotation()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5)
+        {
+            rotate += Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, rotate, 0);
+        }
+    }
+}
