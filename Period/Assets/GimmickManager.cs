3257b6b0259824ef8fc5beb83c6b7e705d030353
K.Yamashita <Yamashita>
2016-11-06 19:51:29 +0900

    GimmickManagerの追加

---
+++ Period/Assets/GimmickManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GimmickManager : MonoBehaviour {
+   
+
+    [System.Serializable]
+    public class Gimmicks {
+        public GameObject gimmickObjs;
+        public float gimmickPoint;  //指定する数値
+    }
+
+    public Gimmicks[] gimmickList;
+    void Awake () {
+        
+        for (int i = 0; i < gimmickList.Length; i++) {
+            gimmickList[i].gimmickObjs.GetComponent<GimmickState>().gimmickNum = (decimal)gimmickList[i].gimmickPoint;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
