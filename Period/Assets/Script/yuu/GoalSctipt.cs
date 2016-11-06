62daa5669da6209a89972f1f1f34b20b69928e79
増山 <analogwatch24@gmail.com>
2016-11-06 15:09:26 +0900

    [増山][追加]Goal処理を行うGoalScriptの作成。（IsTriggerのオブジェクトに付けるだけでOK）

---
+++ Period/Assets/Script/yuu/GoalSctipt.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoalSctipt : MonoBehaviour {
+    //ステージセレクトに行く前の演出時間設定
+    public float directionTime;
+    private bool Goal;
+
+    void Start()
+    {
+        Goal = false;
+    }
+
+    void Update()
+    {
+        if (Goal == true)
+        {
+            directionTime -= Time.deltaTime;
+
+            if (directionTime <= 0) SceneManager.LoadScene("StageSelect");
+        }
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            Goal = true;
+            player.GetComponent<RoboMove>().enabled = false;
+        }
+    }
+}
