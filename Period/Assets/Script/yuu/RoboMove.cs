7e369f5a08b2d20ecf73e326b934b0ad29dcec54
増山 <analogwatch24@gmail.com>
2016-11-10 17:17:57 +0900

    [増山][変更]Player移動時になる音を移動時のみに

--- Period/Assets/Script/yuu/RoboMove.cs
+++ Period/Assets/Script/yuu/RoboMove.cs
@@ -14,10 +14,14 @@ public class RoboMove : MonoBehaviour {
     public Vector3 DirectMove = Vector3.zero;
 
     public Animator charaAnim;
+
+    private AudioSource MoveSound;
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<CharacterController>();
         SD = GameObject.Find("Main Camera").GetComponent<StartDirection>();
+        SoundManager.PlaySE(SE_Enum.MOVE, this.gameObject);
+        MoveSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -51,7 +55,21 @@ public class RoboMove : MonoBehaviour {
         charaAnim.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")) + Mathf.Abs(Input.GetAxisRaw("Vertical")));
 
         Vector3 MoveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        if (MoveDir != Vector3.zero) SoundManager.PlaySE(SE_Enum.MOVE, this.gameObject);
+        if (MoveDir != Vector3.zero)
+        {
+            if (MoveSound.isPlaying == false)
+            {
+                MoveSound.PlayOneShot(MoveSound.clip);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            MoveSound.Stop();
+        }
 
         //地上に居る時の判定（移動もこの中に入れてもいいかもしれない）
         if (controller.isGrounded)
